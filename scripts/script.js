2542d3cf020437f0c8538c0b882f4b95cdfc0598
Sem Postma <sem.postma@gmail.com>
2017-12-15 01:14:11 +0100

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+---
+---
+function border() {
+
+}
+var members = {
+{% for member in site.data.members %}
+    '{{ member.name }}': '{{ member.github }}',
+{% endfor %}
+}
+
+for (const key in members) {
+    if (members.hasOwnProperty(key)) {
+        const element = members[key];
+        alert(key + ': ' + element);
+    }
+}
+
